6795a7f767e1acef33b95145ca53851d4f1988d5
c4xp <c4xp@msn.com>
2025-06-24 22:37:39 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,33 @@
+//public class HelloWorld {
+// public static void main(String[] args) {
+//  System.out.println("Hello, World!");
+// }
+//}
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class HelloWorld {
- public static void main(String[] args) {
-  System.out.println("Hello, World!");
- }
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+        server.createContext("/", new HelloHandler());
+        server.setExecutor(null); // creates a default executor
+        System.out.println("Server started on port 80");
+        server.start();
+    }
+
+    static class HelloHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello from Java HTTP Server!";
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        }
+    }
 }

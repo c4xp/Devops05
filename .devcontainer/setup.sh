0685905a5d03e492771b7b1001401595b4551619
c4xp <c4xp@msn.com>
2025-06-24 19:46:23 +0300

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 set -e
 
 echo "[INFO] Installing dependencies..."
-apt-get update
-apt-get install -y curl git sudo bash iptables conntrack socat ebtables apt-transport-https ca-certificates gnupg2
+sudo apt-get update
+sudo apt-get install -y curl git bash iptables conntrack socat ebtables apt-transport-https ca-certificates gnupg2
 
 echo "[INFO] Installing k3d..."
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
@@ -12,6 +11,9 @@ curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 echo "[INFO] Creating a k3d cluster..."
 k3d cluster create demo-cluster --agents 1
 
+echo "[INFO] Saving kubeconfig..."
 mkdir -p ~/.kube
 k3d kubeconfig get demo-cluster > ~/.kube/config
 k3d kubeconfig get demo-cluster > /workspaces/Devops05/kubeconfig
+
+echo "[INFO] Done! Cluster and config ready."

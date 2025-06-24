6e8a58ea3f12be078c0f1aacfbec80afc3b4f712
c4xp <c4xp@msn.com>
2025-06-24 22:40:39 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,26 +7,28 @@
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
-
-import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class HelloWorld {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
         server.createContext("/", new HelloHandler());
-        server.setExecutor(null); // creates a default executor
-        System.out.println("Server started on port 80");
+        server.setExecutor(null);
+        System.out.println("Server started on port 80...");
         server.start();
     }
 
     static class HelloHandler implements HttpHandler {
-        public void handle(HttpExchange exchange) throws IOException {
-            String response = "Hello from Java HTTP Server!";
-            exchange.sendResponseHeaders(200, response.length());
-            try (OutputStream os = exchange.getResponseBody()) {
+        public void handle(HttpExchange t) {
+            try {
+                String response = "Hello, World!";
+                t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
                 os.write(response.getBytes());
+                os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
